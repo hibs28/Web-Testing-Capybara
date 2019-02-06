00cef8f0bdc72ea65feac8681a7756563a4a5112
Tech-A06 <hibah28@live.co.uk>
2019-02-06 12:03:25 +0000

    Created the confgiuration for capybara in the spec_helper.rb. This sets up the driver and show all hidden files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,17 @@
+require 'capybara'
 require_relative '../lib/sparta_demosite'
 
 RSpec.configure do |config|
   config.formatter = :documentation
   config.color = true
+end
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
+Capybara.configure do |config|
+  config.ignore_hidden = false
+  config.default_max_wait_time = 10
+  config.default_driver = :chrome
 end
