e0046c8ba87e88247b0b4de9e8cfd624e049a64c
Tech-A06 <hibah28@live.co.uk>
2019-02-06 16:26:27 +0000

    Created methods to enter fields in last name, age, dob and can select gender radio button. In registration_spec calls the methods stated

--- lib/pages/registration.rb
+++ lib/pages/registration.rb
@@ -6,12 +6,33 @@ class Registration
   # Page Objects
   REGISTRATION_PAGE_URL = 'http://localhost:9292/'
   FIRST_NAME_FIELD_ID = 'firstName'
+  LAST_NAME_FIELD_ID = 'lastName'
+  DOB_NAME_FIELD = 'dob'
+  GENDER_BUTTON_ID = 'customRadioInline2'
+
 
   def visit_registration_page
     visit(REGISTRATION_PAGE_URL)
   end
 
-  def fill_in_name_field(name)
+  def fill_in_first_name_field(name)
     fill_in(FIRST_NAME_FIELD_ID, :with => name)
   end
+
+  def fill_in_last_name_field(name)
+    fill_in(LAST_NAME_FIELD_ID, :with => name)
+  end
+
+  def fill_in_age_field(age)
+     find(:xpath, '/html/body/div/form/div[3]/div/input').set(age)
+  end
+
+  def fill_in_dob_field(date)
+    fill_in(DOB_NAME_FIELD, :with => date)
+  end
+
+  def choose_gender
+    find(:xpath, '/html/body/div/form/div[6]').click
+  end
+
 end
--- spec/registration_spec.rb
+++ spec/registration_spec.rb
@@ -6,7 +6,12 @@ describe 'testing correct path for sparta registration' do
     it 'should show correct confirmation on completion' do 
       @sparta_demo_site = SpartaDemoSite.new
       @sparta_demo_site.registration_page.visit_registration_page
-      @sparta_demo_site.registration_page.fill_in_name_field('Hibs')
+      @sparta_demo_site.registration_page.fill_in_first_name_field('Hibs')
+      @sparta_demo_site.registration_page.fill_in_last_name_field('Abid')
+      @sparta_demo_site.registration_page.fill_in_age_field(70)
+      @sparta_demo_site.registration_page.fill_in_dob_field('01/01/2000')
+      @sparta_demo_site.registration_page.choose_gender
+      sleep 10
     end
   end
 end
